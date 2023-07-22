0e52ec3d8d292546bccf32cccd7fe388a5ce30d4
OctavioPrado <octavionndchaves@gmail.com>
2023-07-22 17:16:21 -0300

    First commit

---
+++ converter.py
@@ -0,0 +1,34 @@
+import functions,os
+
+options = {
+    'on_off': True,
+    1: functions.decimal_binary,
+    2: functions.decimal_octal,
+    3: functions.decimal_hex,
+    4: functions.binary_octal,
+    5: functions.binary_decimal,
+    6: functions.binary_hex,
+    7: functions.octal_binary,
+    8: functions.octal_decimal,
+    9: functions.octal_hex,
+    10: functions.hex_binary,
+    11: functions.hex_octal,
+    12: functions.hex_decimal,
+    }
+
+while options['on_off']:
+    try:
+        functions.menu()
+        user = int(input('Select one option: '))
+
+        if user == 0:
+            print('\nGoodbye!\n')
+            options['on_off'] = False
+        else:
+            os.system('clear')
+            options.get(user)()
+
+    except ValueError:
+        print('\nSomenting wrong!')
+    except TypeError:
+        print('Invalid option!')
---
+++ functions.py
@@ -0,0 +1,109 @@
+list_of_numbers = []
+
+def menu():
+    print('''
+Options:
+
+[0]-quit
+
+[1]-decimal to binary [4]-binary to octal   [7]-octal to binary  [10]-hex to binary
+[2]-decimal to octal  [5]-binary to decimal [8]-octal to decimal [11]-hex to octal
+[3]-decimal to hex    [6]-binary to hex     [9]-octal to hex     [12]-hex to decimal
+
+            ''')
+
+#      Decimal functions
+# ---------------------------
+
+def decimal_binary():
+    entry = input('Decimal Numbers: ')
+    list_of_numbers = entry.split()
+    for numbers in list_of_numbers:
+        binary_numbers = bin(int(numbers))[2:]
+        print(f'{numbers} --> {binary_numbers}')
+
+def decimal_octal():
+    entry = input('Decimal Numbers: ')
+    list_of_numbers = entry.split()
+    for numbers in list_of_numbers:
+        octal_numbers = oct(int(numbers))[2:]
+        print(f'{numbers} --> {octal_numbers}')
+
+def decimal_hex():
+    entry = input('Decimal Numbers: ')
+    list_of_numbers = entry.split()
+    for numbers in list_of_numbers:
+        hex_numbers = hex(int(numbers))[2:]
+        print(f'{numbers} --> {hex_numbers}')
+
+#      Binary functions
+# ---------------------------
+
+def binary_octal():
+    entry = input('Binary Numbers: ')
+    list_of_numbers = entry.split()
+    for numbers in list_of_numbers:
+        octal_numbers = oct(int(numbers,2))[2:]
+        print(f'{numbers} --> {octal_numbers}')
+
+def binary_decimal():
+    entry = input('Binary Numbers: ')
+    list_of_numbers = entry.split()
+    for numbers in list_of_numbers:
+        decimal_numbers = int(numbers,2)
+        print(f'{numbers} --> {decimal_numbers}')
+
+def binary_hex():
+    entry = input('Binary Numbers: ')
+    list_of_numbers = entry.split()
+    for numbers in list_of_numbers:
+        hex_numbers = hex(int(numbers,2))[2:]
+        print(f'{numbers} --> {hex_numbers}')
+
+#      Octal functions
+# ---------------------------
+
+def octal_binary():
+    entry = input('Octal Numbers: ')
+    list_of_numbers = entry.split()
+    for numbers in list_of_numbers:
+        binary_numbers = bin(int(numbers,8))[2:]
+        print(f'{numbers} --> {binary_numbers}')
+
+def octal_decimal():
+    entry = input('Octal Numbers: ')
+    list_of_numbers = entry.split()
+    for numbers in list_of_numbers:
+        decimal_numbers = int(numbers,8)
+        print(f'{numbers} --> {decimal_numbers}')
+
+def octal_hex():
+    entry = input('Octal Numbers: ')
+    list_of_numbers = entry.split()
+    for numbers in list_of_numbers:
+        hex_numbers = hex(int(numbers,8))[2:]
+        print(f'{numbers} --> {hex_numbers}')
+
+#      Hex functions
+# ---------------------------
+
+def hex_binary():
+    entry = input('Hex Numbers: ')
+    list_of_numbers = entry.split()
+    for numbers in list_of_numbers:
+        binary_numbers = bin(int(numbers,16))[2:]
+        print(f'{numbers} --> {binary_numbers}')
+
+def hex_octal():
+    entry = input('Hex Numbers: ')
+    list_of_numbers = entry.split()
+    for numbers in list_of_numbers:
+        hex_numbers = oct(int(numbers,16))[2:]
+        print(f'{numbers} --> {hex_numbers}')
+
+def hex_decimal():
+    entry = input('Hex Numbers: ')
+    list_of_numbers = entry.split()
+    for numbers in list_of_numbers:
+        decimal_numbers = int(numbers,16)
+        print(f'{numbers} --> {decimal_numbers}')
